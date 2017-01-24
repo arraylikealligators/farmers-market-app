8f02b46131b609f47f1d012e006917365d84f137
xutopia <xuology@gmail.com>
2017-01-24 10:51:18 -0500

    combined all market ID into 1 file. Need to fetch individual market data

--- Database_Data/fetchFarmersMarketData.js
+++ Database_Data/fetchFarmersMarketData.js
@@ -1,7 +1,6 @@
 // here is the script to make the API requests to Farmer's Market API to grab all market data based on zip code
 var request = require('request');
 var zipCodesNYC = require('./zipCodesNYC');
-<<<<<<< HEAD
 
 exports.fetchAllData = function(zipCodes) {
   // iterate through all zip codes and save response to a unique JSON file
@@ -13,20 +12,18 @@ exports.fetchAllData = function(zipCodes) {
     }).pipe(`${zipCode}_Data.json`);
   })
 }
-=======
-var fs = require('fs');
-// console.log(zipCodesNYC);
 
-exports.fetchAllData = (zipCodes) => {
-  // iterate through all zip codes and save response to a unique JSON file
-  zipCodes.forEach((zipCode) => {
-    request(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${zipCode}`, (error, res, body) => {
-      if(!error) {
-        console.error(error);
-      }
-    }).pipe(fs.createWriteStream(`realData/${zipCode}_Data.json`));
-  })
-}
+// var fs = require('fs');
+//
+// exports.fetchAllData = (zipCodes) => {
+//   // iterate through all zip codes and save response to a unique JSON file
+//   zipCodes.forEach((zipCode) => {
+//     request(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${zipCode}`, (error, res, body) => {
+//       if(!error) {
+//         console.error(error);
+//       }
+//     }).pipe(fs.createWriteStream(`realData/${zipCode}_Data.json`));
+//   })
+// }
 
-// exports.fetchAllData(zipCodesNYC.zipCodesNYC);
->>>>>>> realData
+exports.fetchAllData(zipCodesNYC.zipCodesNYC);
