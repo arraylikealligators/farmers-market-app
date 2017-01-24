89ddfd23f8e05399ce2b8190f4c43a424484ea71
Nino <rekhviasnino@gmail.com>
2017-01-24 18:53:22 -0500

    created server folder

--- routes.js
+++
@@ -1,7 +0,0 @@
-var market = require('./marketController.js')
-
-module.exports = function(app, express) {
-
-	app.get('/api/getAllMarkets/', market.allMarkets);
-
-};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser')
 
+var bodyParser = require('body-parser');
+var morgan = require('morgan')
+
+var port = process.env.PORT || 8080;
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://user1:user1@ds117899.mlab.com:17899/fazzar');
@@ -10,9 +13,12 @@ var app = express();
 var routes = require('./routes.js')(app, express);
 
 
-app.listen(8080);
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
-console.log("server running")
+
+app.listen(port)
+console.log("server running on port " + port);
 
 
 module.exports = app; 
