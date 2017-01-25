cc1b219c3f0a8d487ad30982f3d2a559b00aa51e
Nino <rekhviasnino@gmail.com>
2017-01-25 00:08:23 -0500

    provides bulkData and bulkTesk; bulkTest is a function that when run in node will create new models from bulkData and store it in the database; bulkTest needs a lookover given error related to promises. at this commit, the database is successfully accepting JSON post requests

--- server/marketController.js
+++ server/marketController.js
@@ -3,17 +3,37 @@ var Q = require('q');
 
 
 var getAllFarms = Q.nbind(Market.find, Market);
+var createMarket = Q.nbind(Market.create, Market);
 
 module.exports = {
 
 	allMarkets: function(req,res,next){
-		console.log("allMarkets....")
 		getAllFarms({})
 		  .then(function(farms){
-		  	res.json(farms)
+		  	res.json(farms);
 		  })
 		  .fail(function(error){
 		  	next(error);
 		  });
-	}
+	},
+
+	postMarket: function(req, res){
+	
+		var newMarket;
+		if(req.body.marketdetails){
+			newMarket = {
+				Address: req.body.marketdetails.Address,
+				GoogleLink: req.body.marketdetails.GoogleLink,
+				Products: req.body.marketdetails.Products,
+				Schedule: req.body.marketdetails.Schedule,
+				Lat: req.body.marketdetails.Lat,
+				Long: req.body.marketdetails.Long,
+				Name: req.body.marketdetails.Name
+			};
+			 createMarket(newMarket);
+	
+			 res.send("created");
+		} else{ res.send("bad request!"); }
+		
+		}
 };
--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,8 @@ var market = require('./marketController.js')
 
 module.exports = function(app, express) {
 
-	app.get('/', function(req,res){ console.log(req.cookies); res.send("yo"); console.log(req.cookies); console.log(req.session); console.log("beneath cookies and sessions")});
+
 	app.get('/api/getAllMarkets/', market.allMarkets);
+	app.post('/api/postMarket/', market.postMarket);
 
 };
