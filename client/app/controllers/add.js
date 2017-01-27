b8b6749e9dab2e947397e0b405c0bc77d2051dd8
xutopia <xuology@gmail.com>
2017-01-27 12:30:58 -0500

    commented on what the AddController will do

--- client/app/controllers/add.js
+++ client/app/controllers/add.js
@@ -1,5 +1,5 @@
 angular.module('farmer.add', ['farmer.services'])
 
 .controller('AddController', function($scope, Add) {
-  
+  // here lies the function that will take the form submission from the user and send it off as a POST request to add a new market to the DB
 })
