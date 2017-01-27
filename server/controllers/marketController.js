90a5b280967c01ed6172c83be389d0c19b849ed7
Nino <rekhviasnino@gmail.com>
2017-01-27 05:25:12 -0500

    updates marketController to reflect geospatial functionality

--- server/controllers/marketController.js
+++ server/controllers/marketController.js
@@ -2,8 +2,8 @@ var Market = require('../model/farmModel.js');
 var Q = require('q');
 var rp = require('request-promise');
 var MarketQuery = require('../methods/marketMethods');
-
 var getAllFarms = Q.nbind(Market.find, Market);
+var queryMarkets = Q.nbind(Market.find, Market);
 
 var replaceSpaceInAddress = (address) => {
   return address.split(' ').join('+');
@@ -29,14 +29,46 @@ module.exports = {
 			`https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
 		).then((data) => {
 			var coordinates = JSON.parse(data).results[0].geometry.location;
-			// console.log('successfully got geocode', lat);
+			console.log('successfully got geocode' + coordinates );      
 
-      
+			// var marketsDetails = MarketQuery.fetchMarkets(coordinates);
+			var lng = Number(coordinates.lng);
+		    var lat = Number(coordinates.lat);
+		   console.log("typeof lng,", typeof lng, typeof lat, lng)
+		    var marketsDetails; 
 
-			var marketsDetails = MarketQuery.fetchMarkets(coordinates);
-      res.send(marketsDetails);
-		}).catch((err) => {
-			console.error('Failed', err);
+		    queryMarkets(
+						    	
+				   {
+				     geometry: {
+				        $nearSphere: {
+				           $geometry: {
+				              type : "Point",
+				              coordinates : [ lng, lat ]
+				           },
+				           $minDistance: 0,
+				           $maxDistance: 16093
+				        }
+				     }
+				   }
+				
+		    //   {loc: { $geoWithin: { $centerSphere: [ [ lng, lat ], 15/3963.2 ] } } }
+		    ) 
+		    .then((markets) => {
+		      marketsDetails = markets;
+		      console.log('successful query to mongoDB for markets', marketsDetails, typeof marketsDetails);
+		      res.send(marketsDetails);
+		    })
+		    .catch((err) => {
+		      console.error('Failed', err);
+		    });
+
+			// marketsDetails.then((markets)=> { 
+			// 	console.log("marketsDetails in da club", marketsDetails)
+			// 	console.log(typeof marketsDetails, marketsDetails, "back in the controller");
+			// 	res.send(markets)})
+			// console.log(typeof marketsDetails, marketsDetails, "back in the controller");
+   //    	res.send(marketsDetails);
 		});
 		// some function to get a list of farms that match the DB query
 
