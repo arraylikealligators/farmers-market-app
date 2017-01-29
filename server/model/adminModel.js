d3632d2fc9fb56e78fa7b4b3f3429bb3864ee37a
xutopia <xuology@gmail.com>
2017-01-29 14:22:15 -0500

    added an admin model to the mongoDB

---
+++ server/model/adminModel.js
@@ -0,0 +1,10 @@
+// here is the mongoDB model for admin(s) so that they can login and make changes to the DB
+var mongoose = require('mongoose');
+
+var admin = new mongoose.Schema({
+  name: String,
+  password: String,
+  admin: Boolean
+});
+
+module.exports = mongoose.model('admin', admin);
