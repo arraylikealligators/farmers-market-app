2cff0b7e1d480bfd367b71461408d3065bc4f50a
xutopia <xuology@gmail.com>
2017-01-29 14:48:19 -0500

    add packages for hashing password and sessions. Complete server.js setup to allow authentication

--- server/server.js
+++ server/server.js
@@ -12,9 +12,13 @@ var port = process.env.PORT || 8080;
 
 var mongoURI = process.env.mongoURI || keys.mongoURI;
 
-mongoose.connect(mongoURI);
+// instead of just connecting to mlabs, we will be connecting to databse with a secret attached so jwt can be sent back
+var database = keys.database;
+mongoose.connect(database.database);
 
+app.set('superSecret', database.secret);
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(routes);
 app.use(express.static(path.join(__dirname, '../client')));
