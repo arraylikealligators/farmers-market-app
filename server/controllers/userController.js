ba62dfec218577944fe4e48d0c243c922c54604d
Kevin Fung <kevinmcfung@gmail.com>
2017-02-02 11:08:53 -0500

    routes.js refactor complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,50 +1,43 @@
-// here is the controller (server-side) for the login. A query for the admin will be made and the result of that query will generate a jwt
-var Admin = require('../model/adminModel');
+// here is the controller (server-side) for the login. A query for the user will be made and the result of that query will generate a jwt
+var User = require('../model/userModel');
 var Q = require('q');
 var jwt = require('jwt-simple');
 var util = require('../util/util_functions');
 
-var findAdmin = Q.nbind(Admin.find, Admin);
-var addAdmin = Q.nbind(Admin.create, Admin);
+var findUser = Q.nbind(User.find, User);
+var addUser = Q.nbind(User.create, User);
 
 module.exports = {
-  login: function(req, res, next) {
+  signup: (req, res, next) => {
     var username = req.body.username;
     var password = req.body.password;
-    // console.log('login, looking at username', req.body);
 
-    findAdmin({username: username})
-    .then(function(user) {
-      if(!user) {
-        next(new Error('admin does not exist'));
-      } else {
-        return user[0].comparePassword(password)
-        .then(function(valid) {
-          if(valid) {
-            var token = jwt.encode(user, 'aFarmerSecret');
-            res.json({token: token});
-          } else {
-            return next(new Error('password invalid'));
-          }
-        });
+    User.findOne({username: username})
+    .then(user => {
+      if (!user) {
+
       }
-    })
-    .fail(function(err) {
-      next(err);
     });
+
   },
 
-  setup: function(req, res, next) {
-    // this is a 1-time-use function that will add the admin credentials to the DB
-    var username = 'farmer';
-    var password = 'farmer';
-
-    addAdmin({username: username, password: password})
-    .then(function(success) {
-      res.sendStatus(201);
-    })
-    .catch(function(err) {
-      res.sendStatus(400);
-    });
-  }
+  // login: function(req, res, next) {
+  //   var username = req.body.username;
+  //   var password = req.body.password;
+  //   // console.log('login, looking at username', req.body);
+  //
+  //   findUser({username: username})
+  //   .then(function(user) {
+  //     if(!user) {
+  //       next(new Error('user does not exist'));
+  //     } else {
+  //       return user[0].comparePassword(password)
+  //       .then(function(vser({username: username, password: password})
+  //   .then(function(success) {
+  //     res.sendStatus(201);
+  //   })
+  //   .catch(function(err) {
+  //     res.sendStatus(400);
+  //   });
+  // }
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,67 +6,87 @@ var bcrypt = require('bcrypt-nodejs');
 
 var User = require('../controllers/userController');
 
-// this single, one-time admin setup should only be invoked once. As long as the database has 1 instance of the admin, this route should never ever be visited a second time.
-serverRoutes.route('/setup')
-.get((req, res) => {
-  adminController.setup(req, res);
-});
 
-serverRoutes.route('/api/search')
-.get(/* [,some middleware] */ (req, res) => {
-  // invoke marketController.getRadiusMarkets to make query to DB
-  marketController.getLocationMarkets(req, res);
-});
+module.exports = function(app, passport) {
+  // this single, one-time admin setup should only be invoked once. As long as the database has 1 instance of the admin, this route should never ever be visited a second time.
 
-serverRoutes.route('/api/login')
-.post((req, res) => {
-  adminController.login(req, res);
-});
-
-serverRoutes.route('/api/create')
-.post(/* [,some middleware] */ (req, res) => {
-  // invoke marketController.createMarket to add a new market to DB
-  marketController.createMarket(req, res);
-});
-
-serverRoutes.route('/api/getOne')
-.post((req,res) => {
-
-  marketController.fetchOne(req, res);
-
-});
-
-serverRoutes.route('/api/update')
-.put((req,res)=>{
-
-	marketController.updateOne(req, res);
-});
-
-serverRoutes.route('/api/delete')
-.put((req,res)=>{
-	console.log("in serverRoutes at delete");
-	console.log(req.body, "obj in routes");
-	marketController.delete(req,res);
-});
+  // serverRoutes.route('/setup')
+  app.get('/setup', (req, res) => {
+    adminController.setup(req, res);
+  });
 
 
-serverRoutes.route('/api/add')
-.put((req,res)=>{
-	marketController.addMarket(req,res);
-});
+  // serverRoutes.route('/api/search')
+  app.get('/api/search', /* [,some middleware] */ (req, res) => {
+    // invoke marketController.getRadiusMarkets to make query to DB
+    marketController.getLocationMarkets(req, res);
+  });
 
 
- /************************************
- * new routes for passport and auth *
-************************************/
-serverRoutes.route('/signup')
-.post((req, res) => {
-  User.signup(req.body)
-});
+  // serverRoutes.route('/api/login')
+  app.post('/api/login', (req, res) => {
+    adminController.login(req, res);
+  });
 
-serverRoutes.route('/login');
-serverRoutes.route('/logout');
+
+  // serverRoutes.route('/api/create')
+  app.post('/api/create', /* [,some middleware] */ (req, res) => {
+    // invoke marketController.createMarket to add a new market to DB
+    marketController.createMarket(req, res);
+  });
+
+
+  // serverRoutes.route('/api/getOne')
+  app.post('/api/getOne', (req,res) => {
+
+    marketController.fetchOne(req, res);
+
+  });
+
+
+  // serverRoutes.route('/api/update')
+  app.put('/api/update', (req,res)=>{
+
+    marketController.updateOne(req, res);
+  });
+
+
+  // serverRoutes.route('/api/delete')
+  app.put('/api/delete', (req,res)=>{
+    // console.log("in serverRoutes at delete");
+    console.log(req.body, "obj in routes");
+    marketController.delete(req,res);
+  });
 
 
 
-module.exports = serverRoutes;
+  // serverRoutes.route('/api/add')
+  app.put('/api/add', (req,res)=>{
+    marketController.addMarket(req,res);
+  });
+
+
+  /************************************
+  * new routes for passport and auth *
+  ************************************/
+
+  // serverRoutes.route('/signup')
+  app.post('/signup', (req, res) => {
+    User.signup(req.body)
+  });
+
+
+  // serverRoutes.route('/login');
+  app.post('/login', (req, res) => {
+  });
+
+  // serverRoutes.route('/logout');
+  app.post('/logout', (req, res) => {
+  });
+
+
+
+}
+
+
+// module.exports = serverRoutes; // relic from pre-refactor
