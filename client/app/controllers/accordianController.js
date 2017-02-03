68c0658a1c56b6950957d5c6ef3b90fa29e87ea8
Peter Schussheim <peter@schussheim.com>
2017-02-02 19:28:38 -0500

    accordian properly changes state

--- client/app/controllers/accordianController.js
+++ client/app/controllers/accordianController.js
@@ -1,33 +1,28 @@
 angular.module('farmer.accordianController', ['farmer.services'])
 
-.controller('AccordianCtrl', function ($scope, $location, Search) {
-  var accordianController = this
-  var fetch = function (results) {
-    $scope.results = results;
-  }
-  $scope.addNew = function() {
-    $scope.results.push({
-      market: "New One Created",
-      content: "Dynamically added new one",
-      isOpen: false
-    });
-  }
-})
+  .controller('AccordianCtrl', function ($scope, $location, $mdDialog, Search) {
+    var accordianController = this
 
-  // $scope.submit = () => {
-  //   console.log("Submission sent!")
-  //   Search.search({ address: $scope.address, radius: $scope.radius })
-  //   .then((results) => {
-  //     $location.path('/map');
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
-  // };
-  // const defaultBounds = new google.maps.LatLngBounds( new google.maps.LatLng(-33.8902, 151.1759), new google.maps.LatLng(-33.8474, 151.2631));
-  // const input = document.getElementById('searchTextField');
-  // const options = {
-  //   bounds: defaultBounds,
-  // };
+    $scope.selectedMarketIndex = undefined;
 
+    $scope.selectMarketIndex = function (index) {
+      if ($scope.selectedMarketIndex !== index) {
+        $scope.selectedMarketIndex = index;
+      } else {
+        $scope.selectedMarketIndex = undefined;
+      }
+    };
 
+    var fetch = function (results) {
+      $scope.results = results;
+    }
+    
+    $scope.addNew = function () {
+      $scope.results.push({
+        market: "New One Created",
+        content: "Dynamically added new one",
+        isOpen: false
+      });
+
+    }
+  })
